5fa91dec315e320cb9b9431f7bfffdae1df919bd
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-15 17:25:11 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://eskcurso-spring2-ionic.s3-sa-east-1.amazonaws.com"
 }
