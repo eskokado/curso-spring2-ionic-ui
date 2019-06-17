2336b6ea9329c98447dd6e332cdd630dcd446380
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-17 08:38:28 -0300

    Salvando novo cliente

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,7 +4,6 @@ import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storeage.service";
-import { ResponseType } from "@angular/http";
 
 @Injectable()
 export class ClienteService {
@@ -21,5 +20,16 @@ export class ClienteService {
     getImageFromBucket(id: string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`;
         return this.http.get(url, {responseType : 'blob'});
+    }
+
+    insert(obj : ClienteDTO) {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes`, 
+            obj,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
     }
 }
