c362747df5d9300374c78b81465251fd5be177ce
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-21 11:07:45 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://eskvenda-api.herokuapp.com",
     bucketBaseUrl: "https://eskcurso-spring2-ionic.s3-sa-east-1.amazonaws.com"
 }
